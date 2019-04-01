27b94a19a3554a90d9cae13ef08cd29ae6c58fd1
JUN <soipon0@gmail.com>
2019-04-01 23:48:45 +0900

    Productモデルにバリデーションを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+  # 定数を使ってステータスを管理
+  enum status: { draft: 0, published: 1, closed: 2 }
+
+  validates :name,       presence: true
+  #整数のみを入力してもらうようにバリデーションをかける
+  #値は0より大きいこと,defalutではnil値は許容されないので、allow_ nil:trueをつける
+  validates :selllimit,  numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
+  validates :price,      numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
+  # on: を使って値がなくても一旦保存できるようにする。
+  # ただし公開する場合は必須項目とする
+  validates :description, presence: true, on: :published
+  validates :price,       presence: true, on: :published
+  validates :selllimit,   presence: true, on: :published
+  
 end
--- db/migrate/20190401134125_create_products.rb
+++ db/migrate/20190401134125_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :name
+      t.string :name, null: false
       t.integer :price
       t.text :description
-      t.integer :status
+      t.integer :status, null: false, default: 0
       t.integer :selllimit
 
       t.timestamps
