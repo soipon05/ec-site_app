2a93db3b7b8d7a8e5103aea6f4e0f51850b7968a
JUN <soipon0@gmail.com>
2019-04-01 17:49:09 +0900

    bootstrap導入による変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery3
+//= require jquery_ujs
+//= popper
+//= bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
