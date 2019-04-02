a36ac35ad20b5d5689a12d0dac7ded439ece2431
JUN <soipon0@gmail.com>
2019-04-03 00:05:12 +0900

    products-CにNewアクション&Createアクション追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,25 @@ class ProductsController < ApplicationController
   def show
     @products = Product.find(params[:id])
   end
+  
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to root_url, notice: "製品：「#{@product.name}」を登録しました"
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :status, :selllimit, :image)
+  end
 end
