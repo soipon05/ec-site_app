e7a93e4f001b761d5f7f22faeb44b6fba83d384d
JUN <soipon0@gmail.com>
2019-04-10 20:50:55 +0900

    Cartモデルに製品検索の処理追加

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,13 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_products, dependent: :destroy
   has_many :products, through: :cart_products
+
+  # 自身のカート内の製品一覧を出すため用のメソッド
+  def self.search(user)
+    if user.present?
+      CartProduct.where(cart: user.cart)
+    else
+      nil
+    end
+  end
 end
