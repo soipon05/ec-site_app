cf1b3d6ad347cef64265efc46c337135f3c9c2c4
JUN <soipon0@gmail.com>
2019-04-11 20:24:56 +0900

    カートに製品を追加する処理の追加

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,4 +1,13 @@
 class CartProductsController < ApplicationController
   def create
+    @cart = current_user.cart
+    product = Product.find(Hashids.new("test").decode(params[:product_id])).join)
+    @cart_product = @cart.cart_products
+    if CartProduct.add_product(product.id, @cart)
+      redirect_to carts_path
+    else
+      flash[:error] = 'カートへの追加に失敗しました'
+      redirect_to product_path(product)
+    end
   end
 end
--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,4 +1,19 @@
 class CartProduct < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+  # カートに製品を登録する処理を完結にするためのメソッド
+  # プロダクトIDとカートを引数に持ってくることでカート内の製品を特定するorなければ新しく作成
+  # 作成したカート製品の数量を変更して保存する
+  def self.add_product(product_id, cart)
+    cart = CartProduct.find_or_initialize_by(cart: cart, product_id: product_id)
+    if cart.quntity.nil?
+      cart.quantity = 0
+      cart.quantity += 1
+    else
+      cart.quantity +=1
+    end
+
+    cart.save! && cart
+  end
+  
 end
