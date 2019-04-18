9ffe02239b75bfcb6f7d184cb3fc21db394923ed
JUN <soipon0@gmail.com>
2019-04-18 23:59:32 +0900

    PurchaseRecordに処理を追加

--- app/models/purchase_record.rb
+++ app/models/purchase_record.rb
@@ -1,2 +1,13 @@
 class PurchaseRecord < ApplicationRecord
+   def set_attribute
+    
+    # 請求先と配送先を用意する
+    set_invoice
+    set_delivery
+    
+    # カートから製品情報を取得してdetailを新規作成しておく(未保存)
+    # 作成したdetailから製品の各値を取得して合計請求金額を作成する
+    set_product_from_cart
+    set_amount
+  end
 end
